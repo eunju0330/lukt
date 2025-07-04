b04a6d80cf40a0ef2f45294ad44ccc562dcb2ff8
eunju <echoe7894@gmail.com>
2025-07-04 18:12:37 +0900

    0704 학원

--- script/main.js
+++ script/main.js
@@ -1,24 +1,4 @@
-/* const adSwiper = new Swiper('.adSwiper', {
-    loop: true,
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    },
-});
-
-// 탭 슬라이드
-const tabSwiper = new Swiper('.tabSwiper', {
-    slidesPerView: 1,
-    pagination: {
-    el: '.swiper-pagination',
-    },
-});
-
-// 리뷰 슬라이드
-const reviewSwiper = new Swiper('.reviewSwiper', {
-    slidesPerView: 1,
-    pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    },
-}); */
+let adSwiper = new Swiper('.adSwiper',{
+    direction:'horizontal',
+    autoplay:{delay:1000,}
+})
