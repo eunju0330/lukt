a548ba060974ba7177381be71d5f384af88cbde0
최은주 <echoe7894@gmail.com>
2025-07-06 02:40:16 +0900

    7 비디오 선택자 준비

--- script/main.js
+++ script/main.js
@@ -2,3 +2,11 @@ let adSwiper = new Swiper('.adSwiper',{
     direction:'horizontal',
     autoplay:{delay:1000,}
 })
+new Swiper('.saleSwiper', {
+    slidesPerView: 3,
+    spaceBetween:17,
+    navigation: {
+        nextEl: '.sale_next',
+        prevEl: '.sale_prev',
+    },
+});
