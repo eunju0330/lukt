bbe7446081802f3143247c93547f8e9f747c4d75
최은주 <echoe7894@gmail.com>
2025-07-07 01:57:35 +0900

    0707 7비디오

--- script/main.js
+++ script/main.js
@@ -6,7 +6,24 @@ new Swiper('.saleSwiper', {
     slidesPerView: 3,
     spaceBetween:17,
     navigation: {
-        nextEl: '.sale_next',
-        prevEl: '.sale_prev',
+        nextEl:'.sale_next',
+        prevEl:'.sale_prev',
     },
+});
+const videoLeftSwiper = new Swiper('.videoLeftSwiper', {
+    allowTouchMove:false,
+    effect:'fade',
+});
+
+const videoRightSwiper = new Swiper('.videoSwiper', {
+  initialSlide:3, // Kids가 보이게
+    navigation:{
+        nextEl:'.tab_next',
+        prevEl:'.tab_prev',
+    },
+    on:{
+        slideChange:function(){
+        videoLeftSwiper.slideTo(this.activeIndex);
+        }
+    }
 });
