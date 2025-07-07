c7aa82f0218dd28cc3a0b05b73b147378c13274c
최은주 <echoe7894@gmail.com>
2025-07-08 01:17:32 +0900

    0708 집

---
+++ README.md
@@ -0,0 +1,30 @@
+/* const adSwiper = new Swiper('.adSwiper',{
+    direction:'horizontal',
+    autoplay:{delay:1000,}
+})
+
+const saleSwiper = new Swiper('.saleSwiper', {
+    slidesPerView: 3,
+    spaceBetween:17,
+    navigation: {
+        nextEl:'.sale_next',
+        prevEl:'.sale_prev',
+    },
+});
+
+const videoLeftSwiper = new Swiper('.videoLeftSwiper', {
+    allowTouchMove:false,
+    effect:'fade',
+});
+const videoRightSwiper = new Swiper('.videoSwiper', {
+    initialSlide:3,
+    navigation:{
+        nextEl:'.tab_next',
+        prevEl:'.tab_prev',
+    },
+    on:{
+        slideChange:function(){
+            videoLeftSwiper.slideTo(this.activeIndex);
+        }
+    }
+}); */js대기
--- script/main.js
+++ script/main.js
@@ -1,30 +1,10 @@
-const adSwiper = new Swiper('.adSwiper',{
-    direction:'horizontal',
-    autoplay:{delay:1000,}
-})
-
-const saleSwiper = new Swiper('.saleSwiper', {
-    slidesPerView: 3,
-    spaceBetween:17,
-    navigation: {
-        nextEl:'.sale_next',
-        prevEl:'.sale_prev',
-    },
-});
-
 const videoLeftSwiper = new Swiper('.videoLeftSwiper', {
-    allowTouchMove:false,
-    effect:'fade',
+    effect: 'fade',
+    allowTouchMove: false,
+    initialSlide: 3
 });
-const videoRightSwiper = new Swiper('.videoSwiper', {
-    initialSlide:3,
-    navigation:{
-        nextEl:'.tab_next',
-        prevEl:'.tab_prev',
-    },
-    on:{
-        slideChange:function(){
-            videoLeftSwiper.slideTo(this.activeIndex);
-        }
-    }
+
+const videoSwiper = new Swiper('.videoSwiper', {
+    initialSlide: 3,
+    slidesPerView: 1,
 });
