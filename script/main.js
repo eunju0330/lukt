928e188a24fd5d7f546ec39bbcde53823de632cb
최은주 <echoe7894@gmail.com>
2025-07-20 05:31:23 +0900

    비디오섹션 수정

--- script/main.js
+++ script/main.js
@@ -29,14 +29,18 @@ new Swiper('#reviewSwiper', {
 const videoLeftSwiper = new Swiper('#videoLeftSwiper', {
     initialSlide:3,
     allowTouchMove:false,
+    loop:true,
 });
 const videoSwiper = new Swiper('#videoSwiper', {
     initialSlide:3,
     slidesPerView:1,
     allowTouchMove:false,
+    loop:true,
 });
 const tabPrev = document.querySelector('.tab_prev');
 const tabNext = document.querySelector('.tab_next');
+const tabs = document.querySelectorAll('.tab_menu .tab button');
+
 tabPrev.addEventListener('click', () => {
     videoSwiper.slidePrev();
     videoLeftSwiper.slidePrev();
@@ -45,3 +49,22 @@ tabNext.addEventListener('click', () => {
     videoSwiper.slideNext();
     videoLeftSwiper.slideNext();
 });
+
+function setActiveTab(i) {
+    tabs.forEach((tab, idx) => {
+        tab.classList.toggle('active', idx === i);
+    });
+}
+
+tabs.forEach((tab, i) => {
+    tab.addEventListener('click', () => {
+        videoSwiper.slideToLoop(i);
+        videoLeftSwiper.slideToLoop(i);
+    });
+});
+
+videoSwiper.on('slideChange', () => {
+    setActiveTab(videoSwiper.realIndex);
+});
+
+setActiveTab(videoSwiper.realIndex);
